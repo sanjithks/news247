0719c606e26fe1bf06f66e6a7a462bbe1b489da4
Dr. Sanjith Soman <sanjith@inspirehealthscience.com>
2023-05-12 21:05:18 +0400

    second commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("images"); 
     eleventyConfig.addPassthroughCopy("admin");
 };
